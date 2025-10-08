b7e97bfe72d823f649a7a8807a3df64f6046729c
MU30 <salimusonda1@gmail.com>
2025-10-08 10:54:51 +0000

    it did not change

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -11,7 +11,6 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from '@/components/ui/tooltip';
-import TranslatedText from '@/app/components/translated-text';
 
 export function ModeToggle() {
   const { setTheme, theme } = useTheme();
@@ -28,8 +27,8 @@ export function ModeToggle() {
             }}
             aria-label="Toggle theme"
           >
-            <SunIcon className="h-[1.2rem] w-[1.2rem] scale-100 transition-all dark:scale-0" />
-            <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] scale-0 transition-all dark:scale-100" />
+            <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+            <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
             <span className="sr-only">Toggle theme</span>
           </Button>
         </TooltipTrigger>
